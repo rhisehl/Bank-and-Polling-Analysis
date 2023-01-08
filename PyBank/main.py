e2e0ee5bc18405bba6f613b76d311edf1c551e4a
Rhi Sehl <rhisehl@gmail.com>
2023-01-08 11:36:29 -0500

    Added comments for each section

---
+++ PyBank/main.py
@@ -0,0 +1,28 @@
+# # # Pybank Coding Challenge
+
+# Import modules
+
+# Import the budget data
+
+# Idenitfy variables
+
+# Determine number of months in dataset
+
+# Determine net total amount of profit / losses over entire period
+
+# Determine changes in profit / losses over entire period
+
+# Determine average change in profit / losses
+
+# Determine greatest increase in profits (date and amount) over the entire period
+
+# Determine greatest decrease in profits (date and amount) over the entire period
+
+# Print analysis to screen
+print("Financial Analysis /n  ----------------------------")
+print("Total months: ") #insert variable for total # months
+print("Total: ") #insert total profit/loss
+print("Average Change: ") #insert variable of average change
+print("Greatest Increase in Profits: ") #insert Mon-dd ($xxxxxx)
+print("Greatest Decrease in Profits: ") #insert Mon-dd ($xxxxxx)
+#### consolidate all print statements into a single line with /n statements
